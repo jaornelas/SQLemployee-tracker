e9f47f9eec5ded91d089f6f0219c0dc5c7e45c44
jaornelas <ornelalf001@gmail.com>
2025-02-09 18:14:55 -0600

    add sql queries to queries file and import start logo

--- src/db/db.js
+++ src/db/db.js
@@ -11,6 +11,34 @@ const pool = new Pool({
   port: 5432,
 });
 
+// const initializeDB = async () => {
+//   const schemaPath = path.join(__dirname, 'schema.sql');
+//   const seedsPath = path.join(__dirname, 'seeds.sql');
+
+//   try {
+//     const schema = await fs.promises.readFile(schemaPath, 'utf8');
+//     await pool.query(schema);
+//     console.log("Database schema created.");
+
+//     const seeds = await fs.promises.readFile(seedsPath, 'utf8');
+//     await pool.query(seeds);
+//     console.log("Database seeded");
+//   } catch (err) {
+//     console.error("Error initializing database", err);
+//   }
+// };
+
+// pool.connect()
+//     .then(() => {
+//     console.log("Connected to the Employee Tracking Database");
+//     initializeDB();
+//   })
+//   .catch(err => {
+//     console.error("Connection failed", err);
+//   });
+
+// module.exports = pool; //export the pool for use in other modules
+
 const connectToDb = async () => {
   try {
       await pool.connect();
