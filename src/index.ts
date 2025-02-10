34d3c94fd6665cb4a2990cd92954362e3c303d0d
jaornelas <ornelalf001@gmail.com>
2025-02-10 00:11:45 -0600

    start main menu options

---
+++ src/index.ts
@@ -0,0 +1,86 @@
+import inquirer from 'inquirer';
+import logo from 'asciiart-logo';
+import Database from './db/index.js';
+
+const db = new Database;
+
+init();
+
+function init() {
+    const logoText = logo({ name: 'Employee Manager' }).render();
+    console.log(logoText);
+
+    loadMainMenu();
+}
+
+function loadMainMenu() {
+    inquirer.prompt([
+        {
+            type: 'list', 
+            name: 'choice', 
+            message: 'What would you like to do?', 
+            choices: [
+                {
+                    name: 'View Employees',
+                    value: 'VIEW_EMPLOYEES'
+                },
+                {
+                    name: 'View All Employees By Department',
+                    value: 'VIEW_EMPLOYEES_BY_DEPARTMENT',
+                  },
+                  {
+                    name: 'View All Employees By Manager',
+                    value: 'VIEW_EMPLOYEES_BY_MANAGER',
+                  },
+                  {
+                    name: 'Add Employee',
+                    value: 'ADD_EMPLOYEE',
+                  },
+                  {
+                    name: 'Remove Employee',
+                    value: 'REMOVE_EMPLOYEE',
+                  },
+                  {
+                    name: 'Update Employee Role',
+                    value: 'UPDATE_EMPLOYEE_ROLE',
+                  },
+                  {
+                    name: 'Update Employee Manager',
+                    value: 'UPDATE_EMPLOYEE_MANAGER',
+                  },
+                  {
+                    name: 'View All Roles',
+                    value: 'VIEW_ROLES',
+                  },
+                  {
+                    name: 'Add Role',
+                    value: 'ADD_ROLE',
+                  },
+                  {
+                    name: 'Remove Role',
+                    value: 'REMOVE_ROLE',
+                  },
+                  {
+                    name: 'View All Departments',
+                    value: 'VIEW_DEPARTMENTS',
+                  },
+                  {
+                    name: 'Add Department',
+                    value: 'ADD_DEPARTMENT',
+                  },
+                  {
+                    name: 'Remove Department',
+                    value: 'REMOVE_DEPARTMENT',
+                  },
+                  {
+                    name: 'View Total Utilized Budget By Department',
+                    value: 'VIEW_UTILIZED_BUDGET_BY_DEPARTMENT',
+                  },
+                  {
+                    name: 'Quit',
+                    value: 'QUIT',
+                  }
+            ]
+        }
+    ])
+}
